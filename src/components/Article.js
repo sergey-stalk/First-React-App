358e13f2be60a428fcd473321f15fa7d821de9e3
Sergey Matveev <s_matveev95@mail.ru>
2018-04-06 16:34:53 +0300

    Finished my first react app +

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,63 +1,67 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 
+class Article extends PureComponent {
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            count: 0
+        }
+    }
 
-class Article extends Component {
-	constructor (props){
-		super(props)
+/*
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state.isOpen !== nextState.isOpen
+    }
+*/
 
-		this.state = {
-			isOpen: props.defaultOpen
-		}
+    componentWillMount() {
+        console.log('---', 'mounting')
+    }
 
-		this.handleClick = handleClick.bind(this)
-	}
+/*
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+            isOpen: nextProps.defaultOpen
+        })
+    }
+*/
 
-	/*shouldComponentUpdate (nextProps, nextState) {
-		return this.state.isOpen !== nextState.isOpen
-	}*/
+    componentWillUpdate() {
+        console.log('---', 'will update')
+    }
 
-	componentWillMount() {
-		console.log('---', 'mounting')
-	}
+    render() {
+        const {article, isOpen, onButtonClick} = this.props
+        const style = {width: '50%'}
+        const body = isOpen && <section className="card-text">{article.text}</section>
+        return (
+            <div className="card mx-auto" style = {style}>
+                <div className="card-header">
+                    <h2 onClick = {this.incrementCounter}>
+                        {article.title}
+                        clicked {this.state.count}
+                        <button onClick={onButtonClick} className="btn btn-primary btn-lg float-right">
+                            {isOpen ? 'close' : 'open'}
+                        </button>
+                    </h2>
+                </div>
+                <div className="card-body">
+                    <h6 className="card-subtitle text-muted">
+                        creation date: {(new Date(article.date)).toDateString()}
+                    </h6>
+                    {body}
+                </div>
+            </div>
+        )
+    }
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
-			isOpen: nextProps.defaultOpen
-		})
-	}
-
-	componentWillUpdate () {
-		console.log('---', 'will Update')
-	}
-	render () {
-		const style = {width: '50%'}
-		const {article} = this.props
-		const body = this.state.isOpen &&<section className='card-text'>{article.text}</section>
-		return (
-			<div className='card mx-auto' style = {style}>
-				<div className='card-header'>
-					<h2>
-						{article.title}
-						<button className='btn btn-primary btn-lg float-right' onClick={this.handleClick}>{this.state.isOpen ? 'close' : 'open'}</button>
-					</h2>
-				</div>
-				<div className='card-body'>
-					{body}
-					<h6 className='card-subtitle text-muted'>
-						creation date: {(new Date(article.date).toDateString())}
-					</h6>
-				</div>
-			</div>
-		)
-	}
+    incrementCounter = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
 }
 
-
-function handleClick() {
-	this.setState ({
-		isOpen: !this.state.isOpen
-	})
-}
 
 export default Article
